c539f6771c18ed16de234902d151e7768b53724d
zlocate <zlocated@gmail.com>
2022-08-18 00:56:52 +0300

    feat: added example

--- frontend/src/client/serviceWorker.js
+++
@@ -1,8 +0,0 @@
-export const serviceWorker = () => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker
-            .register('./sw.js', { scope: '/' })
-            .then(() => console.log('registered'))
-            .catch((error) => console.error('Service worker error', error));
-    }
-};
--- frontend/src/client/ssr.tsx
+++
@@ -1,3 +0,0 @@
-import Page from './layouts';
-
-export default Page;
